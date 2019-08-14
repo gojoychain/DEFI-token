6ab67a0424d59ac0f728cf8d3acabb526779003c
Deric Walintukan <deric.walintukan@gmail.com>
2019-08-14 12:28:27 +0700

    Implement exchange fallback func

---
+++ contracts/lib/ByteUtils.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.11;
+
+library ByteUtils {
+    /// @dev Slices a bytes array based on the starting offset and length.
+    /// @param b The bytes array to slice.
+    /// @param start The starting offset to slice.
+    /// @param len The length of bytes to slice.
+    /// @return The sliced bytes.
+    function sliceBytes(
+        bytes memory b,
+        uint start,
+        uint len)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        require(b.length >= start + len);
+
+        bytes memory newBytes = new bytes(len);
+        for (uint i = 0; i < len; i++) {
+            newBytes[i] = b[start + i];
+        }
+        return newBytes;
+    }
+
+    function isEmpty(bytes32 b) internal pure returns (bool) {
+        return b == 0x0;
+    }
+}
--- contracts/token/DEFI.sol
+++ contracts/token/DEFI.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.5.11;
 
+import "./IJRC223.sol";
 import "./JRC223.sol";
 import "../lib/Ownable.sol";
+import "../lib/ByteUtils";
+import "../lib/SafeMath";
 
 /**
  * @title DEFI token contract.
@@ -10,6 +13,11 @@ import "../lib/Ownable.sol";
  *      5% of JUSD will go to owner of DEFI smart contract.
  */
 contract DEFI is JRC223, JRC223Receiver, Ownable {
+    using ByteUtils for bytes;
+    using SafeMath for uint;
+
+    uint8 private constant OWNER_PERCENTAGE = 5;
+
     string internal _name = "DEFI token";
     string internal _symbol = "DEFI";
     uint8 internal _decimals = 18;
@@ -19,6 +27,7 @@ contract DEFI is JRC223, JRC223Receiver, Ownable {
     /**
      * @dev Creates the token.
      * @param owner Owner of the contract.
+     * @param jusdToken Address of JUSD token contract.
      */
     constructor(
         address owner,
@@ -38,57 +47,34 @@ contract DEFI is JRC223, JRC223Receiver, Ownable {
         external
     {
         require(msg.sender == _jusdToken, "Only JUSD is accepted");
+
+        bytes memory funcHash = data.sliceBytes(0, 4);
+        bytes32 encodedFunc = keccak256(abi.encodePacked(funcHash));
+        if (encodedFunc == keccak256(abi.encodePacked(hex"045d0389"))) {
+            exchange(from, amount);
+        } else {
+            revert("Unhandled function in tokenFallback");
+        }
     }
 
     /**
-     * @dev Mints an amount of the token and assigns it to an account. 
-     *      This encapsulates the modification of balances such that the proper 
-     *      events are emitted.
-     * @param account The account that will receive the created tokens.
-     * @param value The amount that will be created.
+     * @dev Exchanges JUSD for DEFI. Increments balances and transfers some JUSD to owner.
+     * @param exchanger Address of the exchanger.
+     * @param amount Amount being exchanged.
      */
-    function mint(
-        address account, 
-        uint256 value) 
-        public
-        onlyOwner
-        validAddress(account)
-        returns (bool success)
-    {
-        require(value > 0, "value must be greater than 0.");
+    function exchange(address exchanger, uint amount) private {
+        require(amount > 0, "amount should be greater than 0");
 
-        _totalSupply = _totalSupply.add(value);
-        _balances[account] = _balances[account].add(value);
+        // Mint new DEFI
+        _totalSupply = _totalSupply.add(amount);
+        _balances[exchanger] = _balances[exchanger].add(amount);
+
+        // Calculate JUSD going to owner and transfer
+        uint transferAmt = amount.mul(uint256(OWNER_PERCENTAGE)).div(100);
+        IJRC223(_jusdToken).transfer(owner(), transferAmt);
 
         bytes memory empty;
-        emit Transfer(address(0), account, value);
-        emit Transfer(address(0), account, value, empty);
-
-        return true;
-    }
-
-    /**
-     * @dev Burns an amount of the token of a given account.
-     * @param account The account whose tokens will be burnt.
-     * @param value The amount that will be burnt.
-     */
-    function burn(
-        address account, 
-        uint256 value) 
-        public 
-        onlyOwner
-        validAddress(account)
-        returns (bool success)
-    {
-        require(value > 0, "value must be greater than 0.");
-
-        _totalSupply = _totalSupply.sub(value);
-        _balances[account] = _balances[account].sub(value);
-        
-        bytes memory empty;
-        emit Transfer(account, address(0), value);
-        emit Transfer(account, address(0), value, empty);
-
-        return true;
+        emit Transfer(address(0), exchanger, amount);
+        emit Transfer(address(0), exchanger, amount, empty);
     }
 }
