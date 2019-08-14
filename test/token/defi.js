05e60849604d26c4a55f813ed826ec8666cef5c2
Deric Walintukan <deric.walintukan@gmail.com>
2019-08-14 14:44:34 +0700

    Finish constructor tests

--- test/token/defi.js
+++ test/token/defi.js
@@ -33,7 +33,7 @@ contract('DEFI', (accounts) => {
     await timeMachine.revert()
   })
 
-  describe.only('constructor', async () => {
+  describe('constructor', async () => {
     it('should initialize all the values correctly', async () => {
       assert.equal(await defiMethods.owner().call(), ACCT0)
       assert.equal(await defiMethods.name().call(), 'DEFI Token')
@@ -43,161 +43,24 @@ contract('DEFI', (accounts) => {
       assert.equal(await defiMethods.jusdToken().call(), jusd._address)
     })
 
-    it('should emit both Transfer events', async () => {
-      sassert.event(token, 'Transfer(address,address,uint256)')
-      sassert.event(token, 'Transfer(address,address,uint256,bytes)')
-    })
-
     it('throws if owner is not valid', async () => {
-      try {
-        await token.deploy({
-          data: JUSD.bytecode,
-          arguments: [
-            TOKEN_PARAMS.name,
-            TOKEN_PARAMS.symbol, 
-            TOKEN_PARAMS.decimals,
-            TOKEN_PARAMS.totalSupply,
-            INVALID_ADDR,
-          ],
-        }).send({ from: OWNER, gas: 4712388 })
-      } catch (e) {
-        sassert.revert(e)
-      }
+      defi = new web3.eth.Contract(DEFI.abi)
+      await sassert.revert(
+        defi.deploy({
+          data: DEFI.bytecode,
+          arguments: [INVALID_ADDR, jusd._address],
+        }).send({ from: ACCT0, gas: MAX_GAS }),
+        'Requires valid address.')
     })
 
-    it('throws if name is empty', async () => {
-      try {
-        await token.deploy({
-          data: JUSD.bytecode,
-          arguments: [
-            '',
-            TOKEN_PARAMS.symbol, 
-            TOKEN_PARAMS.decimals,
-            TOKEN_PARAMS.totalSupply,
-            TOKEN_PARAMS.owner,
-          ],
-        }).send({ from: OWNER, gas: 4712388 })
-      } catch (e) {
-        sassert.revert(e)
-      }
-    })
-
-    it('throws if symbol is empty', async () => {
-      try {
-        await token.deploy({
-          data: JUSD.bytecode,
-          arguments: [
-            TOKEN_PARAMS.name, 
-            '',
-            TOKEN_PARAMS.decimals,
-            TOKEN_PARAMS.totalSupply,
-            TOKEN_PARAMS.owner,
-          ],
-        }).send({ from: OWNER, gas: 4712388 })
-      } catch (e) {
-        sassert.revert(e)
-      }
-    })
-  })
-
-  describe('mint', () => {
-    it('mints new tokens and increases supply', async () => {
-      await token.methods.mint(TOKEN_PARAMS.owner, 1).send({ from: OWNER })
-      assert.equal(
-        await token.methods.totalSupply().call(),
-        TOKEN_PARAMS.totalSupply + 1
-      )
-      assert.equal(
-        await token.methods.balanceOf(TOKEN_PARAMS.owner).call(),
-        TOKEN_PARAMS.totalSupply + 1
-      )
-
-      await token.methods.mint(ACCT1, 1).send({ from: OWNER })
-      assert.equal(
-        await token.methods.totalSupply().call(),
-        TOKEN_PARAMS.totalSupply + 2
-      )
-      assert.equal(await token.methods.balanceOf(ACCT1).call(), 1)
-    })
-
-    it('emits both Transfer events', async () => {
-      const receipt = await token.methods.mint(
-        TOKEN_PARAMS.owner,
-        1,
-      ).send({ from: OWNER })
-      sassert.event(receipt, 'Transfer', 2)
-    })
-
-    it('throws if the account address is not valid', async () => {
-      try {
-        await token.methods.mint(INVALID_ADDR, 1).send({ from: OWNER })
-      } catch (e) {
-        sassert.revert(e)
-      }
-    })
-
-    it('throws if trying to mint from a non-owner address', async () => {
-      assert.notEqual(await token.methods.owner().call(), ACCT1)
-      try {
-        await token.methods.mint(TOKEN_PARAMS.owner, 1).send({ from: ACCT1 })
-      } catch (e) {
-        sassert.revert(e)
-      }
-    })
-
-    it('throws if trying to mint 0', async () => {
-      try {
-        await token.methods.mint(TOKEN_PARAMS.owner, 0).send({ from: OWNER })
-      } catch (e) {
-        sassert.revert(e)
-      }
-    })
-  })
-
-  describe('burn', () => {
-    it('burns existing tokens and decreases supply', async () => {
-      await token.methods.burn(TOKEN_PARAMS.owner, 1).send({ from: OWNER })
-      assert.equal(
-        await token.methods.totalSupply().call(),
-        TOKEN_PARAMS.totalSupply - 1
-      )
-      assert.equal(
-        await token.methods.balanceOf(TOKEN_PARAMS.owner).call(),
-        TOKEN_PARAMS.totalSupply - 1
-      )
-    })
-
-    it('emits both Transfer events', async () => {
-      const receipt = await token.methods.burn(
-        TOKEN_PARAMS.owner,
-        1,
-      ).send({ from: OWNER })
-      sassert.event(receipt, 'Transfer', 2)
-    })
-
-    it('throws if the account address is not valid', async () => {
-      try {
-        await token.methods.burn(INVALID_ADDR, 1).send({ from: OWNER })
-      } catch (e) {
-        sassert.revert(e)
-      }
-    })
-
-    it('throws if trying to burn from a non-owner address', async () => {
-      assert.notEqual(await token.methods.owner().call(), ACCT1)
-      try {
-        await token.methods.burn(TOKEN_PARAMS.owner, 1).send({ from: ACCT1 })
-      } catch (e) {
-        sassert.revert(e)
-      }
-    })
-
-    it('throws if trying to burn 0', async () => {
-      try {
-        await token.methods.burn(TOKEN_PARAMS.owner, 0).send({ from: OWNER })
-      } catch (e) {
-        sassert.revert(e)
-      }
+    it('throws if jusdToken is not valid', async () => {
+      defi = new web3.eth.Contract(DEFI.abi)
+      await sassert.revert(
+        defi.deploy({
+          data: DEFI.bytecode,
+          arguments: [ACCT0, INVALID_ADDR],
+        }).send({ from: ACCT0, gas: MAX_GAS }),
+        'Requires valid address.')
     })
   })
 })
